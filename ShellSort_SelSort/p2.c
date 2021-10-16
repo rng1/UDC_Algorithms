e4f9892fe1d1239f782c325e867dbc8021c9e1f9
guillermofersan <34185454+guillermofersan@users.noreply.github.com>
2021-10-16 21:18:50 +0200

    P2 empezado

---
+++ ShellSort_SelSort/p2.c
@@ -0,0 +1,270 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#include <stdbool.h>
+#include <math.h>
+
+#define K 100
+
+
+void shell_sort(int v[], int n)
+{
+    int increment = n;
+    int i, j; // for loop variables
+    int aux = 0; // auxiliary variable
+    bool stay;
+    do
+    {
+        increment = increment / 2;
+        for (i = increment + 1; i < n; i++){
+            aux = v[i];
+            j = i;
+            stay = true;
+
+            while ((j - increment) >= 0 && stay)
+            {
+                if (aux < v[j - increment])
+                {
+                    v[j] = v[j - increment];
+                    j = j - increment;
+                }
+                else
+                {
+                    stay = false;
+                }
+            }
+            v[j] = aux;
+        }
+    }
+    while (increment != 1);
+}
+
+void sel_sort(int v[], int n) {
+    int minj,minx;
+
+    for (int i=0; i < n-1;++i){
+        minj=i;
+        minx=v[i];
+        for (int j = i+1; j < n; ++j) {
+            if (v[j]<minx ){
+                minj=j;
+                minx=v[j];
+            }
+        }
+        v[minj]=v[i];
+        v[i]=minx;
+    }
+}
+
+
+
+void init_seed()
+{
+    srand(time(NULL));
+}
+
+void random_init(int v [], int n)
+{
+    int i, m=2*n+1;
+    for (i=0; i < n; i++)
+        v[i] = (rand() % m) - n;
+}
+
+void print_array(int v[], int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%3d ", v[i]);
+    }
+    printf("\n");
+}
+
+
+void sel_sortTest(){
+    int max = 17;
+
+    int randomArray[max], descending[10] = {10,9,8,7,6,5,4,3,2,1};
+
+    printf("Selection sort test:\n");
+    random_init(randomArray, max);
+    printf("Random initialization\n");
+    print_array(randomArray, max);
+    //function isSorted
+    sel_sort(randomArray,max);
+    print_array(randomArray, max);
+    //function isSorted
+
+    printf("Descending initialization\n");
+    print_array(descending, 10);
+    //function isSorted
+    sel_sort(descending,10);
+    print_array(descending, 10);
+    //function isSorted
+}
+
+void shell_sortTest(){
+    int max = 17;
+
+    int randomArray[max], descending[10] = {10,9,8,7,6,5,4,3,2,1};
+
+    printf("\nShell sort test:\n");
+    random_init(randomArray, max);
+    printf("Random initialization\n");
+    print_array(randomArray, max);
+    //function isSorted
+    shell_sort(randomArray,max);
+    print_array(randomArray, max);
+    //function isSorted
+
+    printf("Descending initialization\n");
+    print_array(descending, 10);
+    //function isSorted
+    shell_sort(descending,10);
+    print_array(descending, 10);
+    //function isSorted
+}
+
+//Obtains the system time in microseconds
+double microseconds()
+{
+    struct timeval t;
+    if (gettimeofday(&t, NULL) < 0 )
+        return 0.0;
+    return (t.tv_usec + t.tv_sec * 1000000.0);
+}
+
+void generateAscending(int v[], int n){
+    for (int i = 0; i < n; ++i) {
+        v[i]=i;
+    }
+}
+
+void generateDescending(int v[], int n){
+    n-=1;
+    for (int i = 0; i <= n; ++i) {
+        v[i]=n-i;
+    }
+}
+
+double time_selSortRand(int MAX)
+{
+    int v[MAX], i;
+
+    double ta, tb, t, t1, t2;
+
+    random_init(v,MAX);
+    ta = microseconds();
+
+    //maxSubSum1(v,MAX);
+    sel_sort(v,MAX);
+
+    tb = microseconds();
+    t = tb - ta;
+
+    if(t < 500){
+        ta = microseconds();
+        for(i =0; i < K; i++){
+            random_init(v,MAX);
+
+            //maxSubSum1(v,MAX);
+            sel_sort(v,MAX);
+
+        }
+        tb = microseconds();
+        t1 = tb - ta; //more than 500
+        ta = microseconds();
+
+
+        for(i =0; i < K; i++){
+            random_init(v,MAX);
+        }
+
+        tb = microseconds();
+        t2 = tb - ta;
+        t = (t1 -t2) / K;
+    }
+    return t;
+}
+
+
+
+double measure_time_ShellSort(int MAX)
+{
+    int v[MAX], i;
+    double ta, tb, t, t1, t2;
+
+    random_init(v,MAX);
+    ta = microseconds();
+
+    //maxSubSum2(v,MAX);
+    //poner shell_sort(v,MAX);
+
+    tb = microseconds();
+    t = tb - ta;
+
+    if(t < 500){
+        ta = microseconds();
+        for(i =0; i < K; i++){
+
+            random_init(v,MAX);
+            //maxSubSum2(v,MAX);
+            //poner shell_sort(v,MAX);
+        }
+        tb = microseconds();
+        t1 = tb - ta; //more than 500
+        ta = microseconds();
+
+        for(i =0; i < K; i++){
+            random_init(v,MAX);
+        }
+
+        tb = microseconds();
+        t2 = tb - ta;
+        t = (t1 -t2) / K;
+    }
+    return t;
+}
+
+
+
+
+
+
+int main() {
+
+    int n=500, i;
+    double t;
+
+    init_seed();
+
+
+    sel_sortTest();
+    shell_sortTest();
+
+
+    printf("\n\nSelection sort random:\n");
+    printf("%6s%18s%18s%18s%18s\n", "n", "t(n)", "t(n)/n^1.8", "t(n)/n^2.0", "t(n)/n^2.2");
+    for (i = 0; i<=6;i++){
+        t = time_selSortRand(n);
+        printf("%6d%18.3lf%18.6lf%18.6lf%18.6lf\n",n, t, t/(pow(n,1.8)),t/(pow(n,2)),t/(pow(n,2.2)));
+        n=n*2;
+    }
+
+    //sel sort ascending
+    //sel sort descending
+
+
+    //shell sort random (funcion ya creada mas o menos)
+    //shell sort ascending
+    //shell sort descending
+
+    // agrupar shell sort y sel sort en funciones
+
+
+
+
+    return 0;
+}
+
