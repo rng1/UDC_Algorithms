44b28956905843545a00299e009469a349ba5dc0
Rodrigo Naranjo González <56093078+rng1@users.noreply.github.com>
2021-12-04 08:39:05 +0100

    Add files via upload

--- Prim/prim.c
+++ Prim/prim.c
@@ -1,13 +1,40 @@
+/*
+ *    TITLE: PRIM'S ALGORITHM
+ * SUBTITLE: Practical 4
+ * AUTHOR 1: Martin do Rio Rico          LOGIN 1: martin.dorio
+ * AUTHOR 2: Rodrigo Naranjo Gonzalez    LOGIN 2: r.naranjo
+ * AUTHOR 3: Guillermo Fernandez Sanchez LOGIN 3: guillermo.fernandezs
+ *    GROUP: 6.1
+ *     DATE: 07 / 12 / 21
+ */
+
+#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#include <stdbool.h>
 #include <malloc.h>
 
 #include "queue.h"
 
-// TODO(roi) implementar el main y acoplar las funciones del pdf, revisar
+// TODO(roi) revisar el main y asegurarse de que funcione, también añadir las funciones de los tiempos, lo siento chicos :( 
+void prim(matrix m, int nodes, queue *edges);
+matrix create_matrix(int n);
+void init_matrix(matrix m, int n);
+void free_matrix(matrix m, int n);
 
 int main()
 {
+    queue edges;
 
+    matrix m = create_matrix(5);
+    init_matrix(m, 5);
+
+    prim(m, 5, edges);
+    show_queue(edges);
+    free_matrix(m, 5);
+
+    return 0;
 }
 
 void prim(matrix m, int nodes, queue *edges) 
@@ -59,4 +86,49 @@ void prim(matrix m, int nodes, queue *edges)
 
     free(closest); 
     free(minDistance);
+}
+
+matrix create_matrix(int n)
+{
+    int i;
+    matrix aux;
+
+    if ((aux = malloc(n * sizeof(int *))) == NULL)
+        return NULL;
+
+    for (i = 0; i < n; ++i)
+        if ((aux[i] = malloc(n * sizeof(int))) == NULL)
+            return NULL;
+
+    return aux;
+}
+
+// create an undirected complete graph with random values between int i, j
+void init_matrix(matrix m, int n)
+{
+    int i, j;
+
+    for (i = 0; i < n; ++i)
+        for (j = i + 1; j < n; ++j)
+            m[i][j] = rand() % n + 1;
+
+    for (i = 0; i < n; ++i)
+    {
+        for (j = 0; j <= i; ++j)
+        {
+            if (i == j)
+                m[i][j] = 0;
+            else
+                m[i][j] = m[j][i];
+        }
+    }
+}
+
+void free_matrix(matrix m, int n)
+{
+    int i;
+
+    for (i = 0; i < n; ++i)
+        free(m[i]);
+    free(m);
 }
